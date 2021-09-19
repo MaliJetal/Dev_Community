44c0cee96274bfec4b5f1f8d6909b8daabd1a418
Jetal C. Mali <malijetal1234@gmail.com>
2021-09-19 22:12:33 +0530

    Profile Create Section

--- src/client/App.js
+++ src/client/App.js
@@ -1,27 +1,28 @@
-import React, {Component} from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import {Provider} from 'react-redux';
-import jwt_decode from 'jwt-decode';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+import jwt_decode from "jwt-decode";
 
 //Take store and App.css
-import store from './store';
-import './App.css';
+import store from "./store";
+import "./App.css";
 
-import {setCurrentUser} from './actions/authActions';
-import {logoutUser} from './actions/authActions';
-import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser } from "./actions/authActions";
+import { logoutUser } from "./actions/authActions";
+import setAuthToken from "./utils/setAuthToken";
 
-import PrivateRoute from './components/common/PrivateRoute';
+import PrivateRoute from "./components/common/PrivateRoute";
 
-import Navbar from './components/layout/Navbar';
-import Landing from './components/layout/Landing';
-import Footer from './components/layout/Footer';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
-import Dashboard from './components/dashboard/Dashboard';
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Footer from "./components/layout/Footer";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import Dashboard from "./components/dashboard/Dashboard";
+import CreateProfile from "./components/create-profile/CreateProfile";
 
 //Check for token
-if(localStorage.jwtToken) {
+if (localStorage.jwtToken) {
   //Set auth token haeder auth
   setAuthToken(localStorage.jwtToken);
   //Decode token and get user info and exp
@@ -30,30 +31,35 @@ if(localStorage.jwtToken) {
   store.dispatch(setCurrentUser(decoded));
 
   //Check for expired token
-  const currentTime = Date.now/1000;
-  if(decoded.exp < currentTime){
+  const currentTime = Date.now / 1000;
+  if (decoded.exp < currentTime) {
     //Logout user
     store.dispatch(logoutUser());
 
     //Redirec to login
-    window.location.href = './login';
+    window.location.href = "./login";
   }
 }
 
-class App extends Component{
-  render(){
-    return(
-      <Provider store = {store}>
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
         <Router>
           <div className="App">
             <Navbar />
-            <Route exact path= "/" component={Landing} />
+            <Route exact path="/" component={Landing} />
             <div className="container">
-              <Route exact path = "/login" component={Login}/>
-              <Route exact path = "/register" component= {Register} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/register" component={Register} />
 
               <Switch>
-                <PrivateRoute exact path='/Dashboard' component={Dashboard} />
+                <PrivateRoute exact path="/Dashboard" component={Dashboard} />
+                <PrivateRoute
+                  exact
+                  path="/create-profile"
+                  component={CreateProfile}
+                />
               </Switch>
             </div>
             <Footer />
